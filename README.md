635531d3ba51890e481f932626e9013ec5b18105
Neil Schelly <nschelly@dyn.com>
2016-10-12 12:46:43 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# lemur
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'lemur'
+maintainer 'Neil Schelly'
+maintainer_email 'neil@neilschelly.com'
+license 'apachev2'
+description 'Installs/Configures lemur'
+long_description 'Installs/Configures lemur'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/lemur/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/lemur' if respond_to?(:source_url)
