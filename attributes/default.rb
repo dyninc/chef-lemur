cf965b4f58b410d7d08b5fa9677d766cba764696
Neil Schelly <nschelly@dyn.com>
2016-10-25 17:08:53 -0400

    More configuration control

--- attributes/default.rb
+++ attributes/default.rb
@@ -39,36 +39,45 @@ default["lemur"]["virtualenv"] = {
   "python_provider" => :system
 }
 
+# See documentation for configuration options at:
+# http://lemur.readthedocs.io/en/latest/administration.html#configuration
 default["lemur"]["lemur"] = {
-  "repository" => "https://github.com/neilschelly/lemur",
+  "repository" => "https://github.com/Netflix/lemur",
   "revision" => "HEAD",
   "app" => "/app",
   "config_template_cookbook" => "lemur",
   "config" => {
+    "threads_per_page" => "8",
     "admins" => [''],
     "lemur_restricted_domains" => [],
     "lemur_email" => "lemur@example.com",
+    "lemur_email_sender" => "SMTP",
+    "lemur_default_expiration_intervals" => [30, 15, 2],
     "lemur_security_team_email" => ["lemur-security@example.com"],
     "lemur_default_country" => "country",
     "lemur_default_state" => "state",
     "lemur_default_location" => "location",
     "lemur_default_organization" => "organization",
     "lemur_default_organizational_unit" => "organizational_unit",
+    "lemur_default_issuer_plugin" => "cryptography-issuer",
+    "lemur_allow_weekend_expiration" => "True",
     "active_providers" => [],
     "log_level" => "DEBUG",
     "log_file" => "lemur.log",
+    "debug" => "True",
+    "cors" => "False",
     "sqlalchemy_database_uri" => {
       "scheme" => "postgresql://",
       "user" => "lemur",
       "host" => "localhost",
       "port" => "5432",
       "database" => "lemur"
-    }
+    },
+    "misc_options" => {}
   }
 }
 
 default["lemur"]["nginx"] = {
-  "siteconfig_template" => "site.conf.erb",
-  "siteconfig_template_cookbook" => "lemur",
-
+  "siteconfig_template" => "nginx-site.conf.erb",
+  "siteconfig_template_cookbook" => "lemur"
 }
--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -16,12 +16,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+nc = node["lemur"]["nginx"]
 
 node.default["nginx"]["default_site_enabled"] = false
 include_recipe("nginx::default") if node["lemur"]["feature_flags"]["nginx"]
 
 template "#{node["nginx"]["dir"]}/sites-available/lemur" do
-  source "nginx-site.conf.erb"
+  source nc["siteconfig_template"]
+  cookbook nc["siteconfig_template_cookbook"]
   notifies :restart, "service[nginx]"
 end
 
